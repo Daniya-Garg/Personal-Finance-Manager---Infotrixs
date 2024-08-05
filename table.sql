11949eca31a5e060c0b0491fdfb3e1ade740cb01
Daniya Garg <125795716+Daniya-Garg@users.noreply.github.com>
2024-08-05 10:16:03 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,11 +1,10 @@
-CREATE DATABASE personal_finance_manager;
 USE personal_finance_manager;
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
 
 CREATE TABLE transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -13,5 +12,12 @@ CREATE TABLE transactions (
     date DATE,
     description VARCHAR(255),
     amount DECIMAL(10, 2),
+    category VARCHAR(255),
+    tags VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+
+INSERT INTO users
+VALUES (1,"Daniya","Daniya");
+
